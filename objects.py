374d3944e4b1da4ee1f1821b87c2276e3bfb6e40
viktoria <viktoriazagorodnia@gmail.com>
2015-07-13 22:18:49 +0300

    Созданы обьект Creature, Hero #1

--- objects.py
+++ objects.py
@@ -1,1 +1,12 @@
 #This file will contain object structures
+
+class Creature:
+    Health = "100"
+    Mana = "100"
+	Dmg = "100"
+	Absorb = "100"
+    pass
+
+class Herro(Creature):
+    Stamina = "100"
+    pass
