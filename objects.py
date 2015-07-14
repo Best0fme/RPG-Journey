b25d7b5014f504a453d48d4f7243d550d3c11c89
Illesss <Konovalovillias@gmail.com>
2015-07-14 11:44:05 +0300

    добавил сумку

--- objects.py
+++ objects.py
@@ -1,12 +1,17 @@
 #This file will contain object structures
 
-class Creature:
+class Creature(Object):
     Health = "100"
     Mana = "100"
 	Dmg = "100"
 	Absorb = "100"
     pass
 
-class Herro(Creature):
+class Herro(Creature, Bag):
     Stamina = "100"
+    pass
+
+
+class Bag(Object):
+    articles = []
     pass
