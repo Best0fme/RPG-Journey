2405641f573f77ae9041325b4b1a6d4c48520f21
Trion <sambdima@gmail.com>
2015-07-14 20:03:22 +0300

    #3 + Some changes to #1-#6 Tasks

--- objects.py
+++ objects.py
@@ -1,44 +1,44 @@
 #This file will contain object structures
+class Creature(object):
+    def minus_health(self, health):
+        self._health -= health
 
-class Creature(Object):
-    _health = "100"
-    _mana = "100"
-	_dmg = "100"
-	_absorb = "100"
+    def refill_health(self, health):
+        self._health += health
 
-    pass
-
-class Herro(Creature, Bag):
-    _stamina = "100"
-
-
-    def minus_stamina(self, value):
-        self._stamina -= value
+    def __init__(self, health = 0, mana = 0, dmg = 0, absorb = 0):
+        self._health = health
+        self._mana = mana
+        self._dmg = dmg
+        self._absorb = absorb
 
 
-
-    def refill_stamina(self, value):
-            self._stamina += value
-
-    pass
-
-class Bag(Object):
+class Bag(object):
     _articles_off = []
     _articles_on = []
 
     def wear_item(self, item_index):
-       __value = self._articles_off[item_index].pop
+        __value = self._articles_off.pop(item_index)
         self._articles_on.append(__value)
 
-
     def strip_item(self, item_index):
-        __value = self._articles_on[item_index].pop
+        __value = self._articles_on.pop(item_index)
         self.articles_off.append(__value)
 
-    pass
+
+class Hero(Creature, Bag):
+    def minus_stamina(self, value):
+        self._stamina -= value
+
+    def refill_stamina(self, value):
+            self._stamina += value
+
+    def __init__(self, health = 0, mana = 0, dmg = 0, absorb = 0, stamina = 0):
+        Creature.__init__(self, health, mana, dmg, absorb)
+        self._stamina = stamina
 
 
-class Item(Object):
+class Item(object):
     def __init__(self, health = 0, mana = 0, dmg = 0, absorb = 0, stamina = 0, attack = 0, defence = 0):
         self._health = health
         self._mana = mana
@@ -47,5 +47,3 @@ class Item(Object):
         self._stamina = stamina
         self._attack = attack
         self._defence = defence
-
-    pass
