42943f6bfe94014acdbee916a574bb8e27da620d
Illesss <Konovalovillias@gmail.com>
2015-07-14 12:13:45 +0300

    add item

--- objects.py
+++ objects.py
@@ -1,17 +1,29 @@
 #This file will contain object structures
 
 class Creature(Object):
-    Health = "100"
-    Mana = "100"
-	Dmg = "100"
-	Absorb = "100"
+    _health = "100"
+    _mana = "100"
+	_dmg = "100"
+	_absorb = "100"
     pass
 
 class Herro(Creature, Bag):
-    Stamina = "100"
+    _stamina = "100"
     pass
 
 
 class Bag(Object):
-    articles = []
+    _articles = []
+    pass
+
+
+class Item(Object):
+    def __init__(self, health = 0, mana = 0, dmg = 0, absorb = 0, stamina = 0, attack = 0, defence = 0):
+        self._health = health
+        self._mana = mana
+        self._dmg = dmg
+        self._absorb = absorb
+        self._stamina = stamina
+        self._attack = attack
+        self._defence = defence
     pass
